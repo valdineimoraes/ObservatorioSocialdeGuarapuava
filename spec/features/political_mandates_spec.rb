53f1c0c9d7a5617ddeca5d58240bc0d1d48a2eb9
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2019-06-05 13:56:35 -0300

    mudanças na view

--- spec/features/political_mandates_spec.rb
+++ spec/features/political_mandates_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Political_Mandates', type: :feature do
+
   describe '#create' do
     before(:each) do
       visit new_political_mandate_path
@@ -39,9 +40,93 @@ RSpec.describe 'Political_Mandates', type: :feature do
         expect(page).to have_selector('div.alert.alert-danger',
                                       text: I18n.t('flash.actions.errors'))
 
-        within('div.meeting_start_session') do
-          expect(page).to have_content(I18n.t('errors.messages.less_hour'))
+        within('div.political_mandate_first_period') do
+          expect(page).to have_content(I18n.t('errors.messages.blank'))
         end
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:political_mandate) { create(:political_mandate) }
+
+    before(:each) do
+      visit edit_political_mandate_path(political_mandate)
+    end
+
+    context 'fill fields' do
+      it 'with correct values' do
+        expect(page).to have_field 'political_mandate_description', with: political_mandate.description
+      end
+    end
+
+    context 'with valid fields' do
+      it 'update political_mandate' do
+        new_mandate = 'MandatoXXXX'
+        fill_in 'political_mandate_description', with: new_mandate
+
+        submit_form
+
+        expect(page.current_path).to eq political_mandates_path
+
+        expect(page).to have_selector('div.alert.alert-success',
+                                      text: 'Mandato politico atualizado com sucesso! ')
+
+        within('table tbody') do
+          expect(page).to have_content(new_mandate)
+        end
+      end
+    end
+
+    context 'when invalid fields' do
+      it 'show errors' do
+        fill_in 'political_mandate_description', with: ''
+        submit_form
+
+        expect(page).to have_selector('div.alert.alert-danger',
+                                      text: I18n.t('flash.actions.errors'))
+
+        within('div.political_mandate_description') do
+          expect(page).to have_content(I18n.t('errors.messages.blank'))
+        end
+      end
+    end
+  end
+
+
+  describe '#destroy' do
+    it 'political_mandate' do
+      political_mandate = create(:political_mandate)
+      visit political_mandates_path
+
+      destroy_link = "a[href='#{political_mandate_path(political_mandate)}'][data-method='delete']"
+      find(destroy_link).click
+
+      expect(page).to have_selector('div.alert.alert-success',
+                                    text: 'Mandato politico destruido com sucesso! ')
+
+      within('table tbody') do
+        expect(page).not_to have_content(political_mandate.description)
+      end
+    end
+  end
+
+  describe '#index' do
+    let!(:political_mandates) { create_list(:political_mandate, 3) }
+
+    it 'show all political_mandate with options' do
+      visit political_mandates_path
+
+      political_mandates.each do |political_mandate|
+        expect(page).to have_content(political_mandate.description)
+        expect(page).to have_content(political_mandate.first_period)
+        expect(page).to have_content(political_mandate.final_period)
+
+        expect(page).to have_link(href: edit_political_mandate_path(political_mandate))
+        expect(page).to have_link(href: political_mandate_path(political_mandate))
+
+        destroy_link = "a[href='#{political_mandate_path(political_mandate)}'][data-method='delete']"
+        expect(page).to have_css(destroy_link)
       end
     end
   end
--- spec/models/political_mandate_spec.rb
+++ spec/models/political_mandate_spec.rb
@@ -1,7 +1,14 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe PoliticalMandate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject {create(:political_mandate)}
+    it { is_expected.to validate_presence_of(:description)}
+    it { is_expected.to validate_presence_of(:first_period)}
+    it { is_expected.to validate_presence_of(:final_period)}
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:councilmen).dependent(:destroy) }
+  end
 end
